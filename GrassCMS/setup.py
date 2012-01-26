9bc6217b86ee3e8be96f1b38ccc495f55e8426d6
David Francos (XayOn) <opensource@davidfrancos.net>
2012-01-26 02:17:55 +0100

    Added a setup.py

---
+++ GrassCMS/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# For some reason, building exes with py2exe doesnt work right now.
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+import os
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+scripts = ['grasscms_server.py']
+data_files = []
+
+for dirpath, dirnames, filenames in os.walk('grasscms'):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+setup(name='GrassCMS',
+      version='0.0.1',
+      description='GrassCMS',
+      url='http://www.grasscms.com/',
+      download_url='http://www.grasscms.com',
+      license='GPL2',
+      requires=['sqlalchemy' ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+      ],
+      long_description="GrassCMS, easy-to-use content management system",
+      packages=['grasscms'],
+      scripts=scripts,
+      options=opts,
+      data_files=data_files,
+      package_data={
+        'grasscms' : [
+            'static/'
+            'templates/'
+            ]
+      }
+     )
